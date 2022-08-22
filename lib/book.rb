61a45a5d862ae1f3e1abaa77ddf10ca0a5a36495
Ndalila <vinson.steve@student.moringaschool.com>
2022-08-23 02:51:55 +0300

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,31 @@
+class Book
 
+    attr_writer :author 
+    attr_writer :page_count 
+    attr_writer :genre
+
+    def initialize(title)
+        @title = title
+    end
+
+    def title 
+        @title
+    end
+
+    def author
+        @author
+    end
+
+    def page_count 
+        @page_count
+    end
+
+    def genre 
+        @genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,37 @@
 # Make your shoe class here!
+
+class Shoe 
+
+    attr_writer :color
+    attr_writer :size
+    attr_writer :material
+    attr_accessor :condition
+   def initialize(brand)
+        @brand = brand
+   end 
+
+   def brand 
+    @brand
+   end
+
+   def color  
+    @color
+   end
+
+   def size 
+    @size
+   end
+
+   def material 
+    @material 
+   end
+
+   def condition 
+    @condition
+   end
+
+   def cobble
+    puts "Your shoe is as good as new!"
+    @condition= "new"
+   end
+end
